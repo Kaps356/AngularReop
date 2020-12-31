d37c29ff47abe7e14efb66580321eeeed3545f9b
Kaps356 <kalpesh.shinde356@gmail.com>
2020-12-31 16:29:26 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  title = 'Welcome to Angular';
+  arr = ["Apple", "Orange", "Banana", "Bitrrot"];
+  loginFlag = false;
+  count = 1;
+
+  hello() {
+    this.title = "You clicked button";
+    console.log("hello world");
+    if (this.loginFlag == false) {
+      this.loginFlag = true;
+    } else {
+      this.loginFlag = false;
+    }
+  }
+
+  bye() {
+    console.log("hi this is from text box");
+    this.name = "hi" + this.count;
+    this.count++;
+  }
 }
